a3069a482a0eab059ceefc57b324fc64f1c81724
HoangT1215 <HoangT1215@gmail.com>
2017-04-17 00:48:44 -0700

    writing mean reversion strat

---
+++ mean-reversion.py
@@ -0,0 +1,26 @@
+from event import OrderEvent
+import pandas as pd
+import numpy as np
+
+'''
+Strategy note:
+To do:
+	- How to take different ticks
+	- How ADF tests 
+
+
+
+'''
+
+class meanrevertstrat(object):
+	def __init__():
+		self.instrument = instrument
+        self.units = units
+        self.events = events
+        self.ticks = 0
+        self.data = pd.DataFrame()
+
+	def calculate():
+		pass
+
+	def ADFtest():
