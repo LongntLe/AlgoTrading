0820c826f78d4194e76e5ed6cc28417f6f6645a3
HoangT1215 <HoangT1215@gmail.com>
2017-04-23 18:37:10 -0700

    strategy

--- mean-reversion.py
+++ mean-reversion.py
@@ -1,12 +1,15 @@
 from event import OrderEvent
 import pandas as pd
 import numpy as np
+from statisticaltest import hurst #calculating hurst exponent
 
 '''
 Strategy note:
 To do:
 	- How to take different ticks
-	- How ADF tests 
+		- calculate hurst as test first, can get historical data
+	- How ADF tests
+	-
 
 '''
 
@@ -24,30 +27,3 @@ class meanrevertstrat(object):
 		if event.type == "TICK":
             self.ticks += 1
 		pass
-
-	def ADFtest():
-		pass
-
-	def hurst(context, data, sid):
-    # Gathers all the prices that you need
-    gather_prices(context, data, sid, 40) #need to fetch data somehow
-    # Checks whether data exists
-    data_gathered = gather_data(data)
-    if data_gathered is None:
-        return    
-    
-    tau, lagvec = [], []
-    # Step through the different lags
-    for lag in range(2,20):  
-        # Produce price different with lag
-        pp = numpy.subtract(context.past_prices[lag:],context.past_prices[:-lag])
-        # Write the different lags into a vector
-        lagvec.append(lag)
-        # Calculate the variance of the difference
-        tau.append(numpy.sqrt(numpy.std(pp)))
-    # Linear fit to a double-log graph to get power
-    m = numpy.polyfit(numpy.log10(lagvec),numpy.log10(tau),1)
-    # Calculate hurst
-    hurst = m[0]*2
-    
-    return hurst
