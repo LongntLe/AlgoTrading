62b3fe4395170f67290ef12988b72b253f0a7181
LongntLe <long@minerva.kgi.edu>
2017-04-29 00:02:08 -0700

    update position.py using Decimal

--- position.py
+++ position.py
@@ -1,27 +1,46 @@
+from decimal import Decimal, getcontext, ROUND_HALF_DOWN
+
 class Position(object):
     def __init__(
-            self, side, market, units, 
-            exposure, avg_price, cur_price
+            self, position_type, market, units, 
+            exposure, bid, ask
             ):
-        self.side = side
+        self.position_type = position_type
         self.market = market
         self.units = units
-        self.exposure = exposure
-        self.avg_price = avg_price
-        self.cur_price = cur_price
-        self.profit_base = self.calculate_profit_base()
-        self.profit_perc = self.calculate_profit_perc()
+        self.exposure = Decimal(str(exposure))
+
+        if self.position_type == "long":
+            self.avg_price = Decimal(str(ask))
+            self.cur_price = Decimal(str(bid))
+        else:
+            self.avg_price = Decimal(str(bid))
+            self.cur_price = Decimal(str(ask))
+
+        self.profit_base = self.calculate_profit_base(self.exposure)
+        self.profit_perc = self.calculate_profit_perc(self.exposure)
 
     def calculate_pips(self):
-        mult = 1.0
-        if self.side == "SHORT":
-            mult = -1.0
-        return mult*(self.cur_price - self.avg_price)
+        getcontext.prec = 6
+        mult = Decimal('1')
+        if self.position_type == "long":
+            mult = Decimal('1')
+        elif self.position_type == "short":
+            mult = Decimal("-1")
+        return (mult*(self.cur_price - self.avg_price)).quantize(
+                Decimal("0.00001"), ROUND_HALF_DOWN
+                )
 
-    def calculate_profit_perc(self):
-        return self.profit_base / self.exposure * 100.0
+    def calculate_profit_perc(self, exposure):
+        pips = self.calculate_pips()
+        return (pips * exposure / self.cur_price).quantize(
+                Decimal("0.00001"), ROUND_HALF_DOWN
+                )
 
-    def update_position_price(self, cur_price):
-        self.cur_price = cur_price
-        self.profit_base = self.calculate_profit_base()
-        self.profit_perc = self.calculate_profit_perc()
+    def update_position_price(self, bid, ask, exposure):
+        if self.position_type == "long":
+            self.cur_price = Decimal(str(bid))
+        else:
+            self.cur_price = Decimal(str(ask))
+        self.profit_base = self.calculate_profit_base(exposure)
+        self.profit_perc = self.calculate_profit_perc(exposure)
